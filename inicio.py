f4134c0e707d4076b77512b52d9db50ed8f2c46c
car0lina06 <lauvalcabra@gmail.com>
2024-05-28 08:17:15 -0600

    Nuevo Repositorio

---
+++ inicio.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+import pymysql
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/Perritos')
+def perritos():
+    return render_template("Perritos.html")
+
+@app.route('/Gatito')
+def gatito():
+    return render_template("Gatito.html")
+
+@app.route('/conejito')
+def conejito():
+    return render_template("conejito.html")
+
+@app.route('/comenta')
+def comemta():
+    return render_template("form.html")
+
+@app.route('/agrega_comenta', methods=['POST'])
+def agrega_comenta():
+    if request.method == 'POST':
+        aux_Correo = request.form['correo']
+        aux_Comentarios = request.form['comentarios']
+        conn = pymysql.connect(host='localhost', user='root', passwd='', db='agenda' )
+        cursor = conn.cursor()
+        cursor.execute('insert into comenta (correo,comentarios) values (%s, %s)',(aux_Correo, aux_Comentarios))
+        conn.commit()
+    return redirect(url_for('crud'))
+
+
+
+@app.route('/crud')
+def crud():
+    conn = pymysql.connect(host='localhost', user='root', passwd='', db='agenda2')
+    cursor = conn.cursor()
+    cursor.execute('select id, correo, comentarios from comenta order by id')
+    datos = cursor.fetchall()
+    return render_template("crud.html", comentarios = datos)
+
+@app.route('/editar/<string:id>')
+def editar(id):
+    conn = pymysql.connect(host='localhost', user='root', passwd='', db='agenda2')
+    cursor = conn.cursor()
+    cursor.execute('select id, correo, comentarios from comenta where id = %s', (id))
+    dato  = cursor.fetchall()
+    return render_template("editar.html", comentar=dato[0])
+
+@app.route('/editar_comenta/<string:id>',methods=['POST'])
+def editar_comenta(id):
+    if request.method == 'POST':
+        corr=request.form['correo']
+        come=request.form['comentarios']
+        conn = pymysql.connect(host='localhost', user='root', passwd='', db='agenda2')
+        cursor = conn.cursor()
+        cursor.execute('update comenta set correo=%s, comentarios=%s where id=%s', (corr,come,id))
+        conn.commit()
+    return redirect(url_for('crud'))
+
+@app.route('/borrar/<string:id>')
+def borrar(id):
+    conn = pymysql.connect(host='localhost', user='root', passwd='', db='agenda2')
+    cursor = conn.cursor()
+    cursor.execute('delete from comenta where id = {0}'.format(id))
+    conn.commit()
+    return redirect(url_for('crud'))
+
+@app.route('/insertar')
+def insertar():
+    return render_template("insertar.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
